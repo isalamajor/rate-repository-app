6bd4827e726837b0ee40e207e540370aeea98695
isalamajor <isabel.hernandez7barrio@gmail.com>
2026-02-03 11:36:08 +0100

    eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,32 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
+    plugins: {
+      react: require("eslint-plugin-react"),
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    languageOptions: {
+      parser: require("@babel/eslint-parser"),
+      parserOptions: {
+        requireConfigFile: false,
+        ecmaVersion: "latest",
+        sourceType: "module",
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    },
+    rules: {
+      semi: "error",
+      "react/prop-types": "off",
+    },
   },
 ]);
